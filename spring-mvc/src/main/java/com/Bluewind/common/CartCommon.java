bfa8a2fe015c0301902ca061f5a90c674a30e486
tuannn <tuan21.onion@gmail.com>
2023-10-04 01:25:59 +0700

    update

---
+++ spring-mvc/src/main/java/com/Bluewind/common/CartCommon.java
@@ -0,0 +1,71 @@
+package com.Bluewind.common;
+
+import com.Bluewind.dto.CartProductDetail;
+import com.Bluewind.dto.admin.CartDTO;
+import com.Bluewind.dto.admin.ProductDTO;
+import com.Bluewind.dto.admin.ProductDetailDTO;
+import com.Bluewind.service.ICartService;
+import com.Bluewind.service.IProductDetailService;
+import com.Bluewind.service.IProductService;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Component
+public class CartCommon {
+
+    private final ICartService iCartService;
+    private final IProductDetailService iProductDetailService;
+    private final IProductService iProductService;
+
+    public CartCommon(ICartService iCartService, IProductDetailService iProductDetailService, IProductService iProductService) {
+        this.iCartService = iCartService;
+        this.iProductDetailService = iProductDetailService;
+        this.iProductService = iProductService;
+    }
+
+    public List<CartProductDetail> getCarts(Integer accountId) {
+        List<CartDTO> cartDTOS = iCartService.findAllByAccountID(accountId);
+
+        List<Integer> pdIds = new ArrayList<>();
+        if (!cartDTOS.isEmpty()) {
+            for (CartDTO dto : cartDTOS) {
+                pdIds.add(dto.getProductID());
+            }
+        }
+
+        List<ProductDetailDTO> productDetailDTOS = iProductDetailService.findAllByProductIds(pdIds);
+
+        List<Integer> pIds = new ArrayList<>();
+        if (!productDetailDTOS.isEmpty()) {
+            for (ProductDetailDTO dto : productDetailDTOS) {
+                pIds.add(dto.getProductID());
+            }
+        }
+
+        List<ProductDTO> productDTOS = iProductService.findAllByProductIds(pIds);
+
+        List<CartProductDetail> cartProductDetails = new ArrayList<>();
+
+        for (CartDTO dto : cartDTOS) {
+            ProductDetailDTO productDetailDTO = productDetailDTOS.stream().filter(p -> p.getProductDetailID() == dto.getProductID()).findFirst().get();
+            ProductDTO productDTO = productDTOS.stream().filter(p -> Objects.equals(p.getProductID(), productDetailDTO.getProductID())).findFirst().get();
+
+            CartProductDetail cartProductDetail = CartProductDetail.builder()
+                    .cartId(dto.getCartID())
+                    .accountId(accountId)
+                    .productDetailId(productDetailDTO.getProductDetailID())
+                    .name(productDTO.getProductName())
+                    .size(productDetailDTO.getSize())
+                    .image(productDTO.getImg1())
+                    .price(productDTO.getPrice())
+                    .quantity(dto.getQuantity())
+                    .build();
+            cartProductDetails.add(cartProductDetail);
+        }
+        return cartProductDetails;
+    }
+}
--- spring-mvc/src/main/java/com/Bluewind/controller/user/OrderController.java
+++ spring-mvc/src/main/java/com/Bluewind/controller/user/OrderController.java
@@ -1,5 +1,7 @@
 package com.Bluewind.controller.user;
 
+import com.Bluewind.common.CartCommon;
+import com.Bluewind.dto.CartProductDetail;
 import com.Bluewind.dto.OrderResponse;
 import com.Bluewind.dto.admin.CartDTO;
 import com.Bluewind.dto.admin.OrderDTO;
@@ -9,6 +11,7 @@ import com.Bluewind.service.IOrderDetailService;
 import com.Bluewind.service.IOrderService;
 import com.Bluewind.service.impl.OrderService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,22 +28,20 @@ public class OrderController {
     private final IOrderService orderService;
     private final IOrderDetailService iOrderDetailService;
     private final ICartService iCartService;
+    private final CartCommon cartCommon;
 
-    public OrderController(OrderService orderService, IOrderDetailService iOrderDetailService, ICartService iCartService) {
+    public OrderController(OrderService orderService, IOrderDetailService iOrderDetailService, ICartService iCartService, CartCommon cartCommon) {
         this.orderService = orderService;
         this.iOrderDetailService = iOrderDetailService;
         this.iCartService = iCartService;
+        this.cartCommon = cartCommon;
     }
 
     @RequestMapping(value = "/checkout", method = RequestMethod.GET)
-    public ModelAndView checkout(HttpServletRequest request) {
+    public ModelAndView checkout(@RequestBody Integer accountId) {
         ModelAndView mav = new ModelAndView("");
-        Integer accountId = Integer.parseInt(request.getParameter("accountId"));
-        List<CartDTO> cartDTOS = iCartService.findAllByAccountID(accountId);
-
-
-
-        mav.addObject("cartDTOS", cartDTOS);
+        List<CartProductDetail> cartProductDetails = cartCommon.getCarts(accountId);
+        mav.addObject("cartProductDetails", cartProductDetails);
         return mav;
     }
 
